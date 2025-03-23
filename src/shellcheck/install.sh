865e9dbedbe76d4a239173821f571645f264b81e
Doug Hom <204439961+doughom@users.noreply.github.com>
2025-03-23 23:54:42 +0000

    feat: shellcheck

---
+++ src/shellcheck/install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1091
+set -e
+
+source /usr/local/bin/common-utils.sh
+
+url=$(get_github_asset_url koalaman/shellcheck "$TAG")
+download "$url" localFile
+
+if [[ "$HASH" !=  "none" ]]; then
+  test_file_hash localFile "$HASH"
+fi
+
+tar xf localFile
+find . -type f -name shellcheck -exec install {} /usr/local/bin/shellcheck \;
